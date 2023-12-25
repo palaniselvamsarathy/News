b7f5cc5884f79101a2a78fd833d69e686e16d13a
palaniselvamsarathy <palaniselvamsarathy@gmail.com>
2023-12-26 00:09:57 +0530

    okie

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  // let [articles,setArticles] = useState([])
+  // useEffect(()=>{
+  //   fetch("https://newsapi.org/v2/everything?q=india&from=2023-12-24&apiKey=3370af73c5c345fdb0ec9a05246510f0")
+  //   .then((response)=>response.json())
+  //   .then((news)=>{
+  //     setArticles(news.articles);
+  //   })
+  //   .catch((err)=>{
+  //     console.log(err)
+  //   })
+  // },[])
   return (
     <div className="App">
       <h1>News App</h1>
